c568892253035088c2aac75c59dda9f270e54af5
mdasif8393 <md.asif.8393@gmail.com>
2022-10-20 13:40:58 +0600

    add 1.SyncAsync.js

--- 1.SyncAsync.js
+++ 1.SyncAsync.js
@@ -1,6 +1,3 @@
-
 console.log("Welcome to ACC");
-
 setTimeout(()=>console.log("I am excited to teach"), 1000);
-
 console.log("Hope you are also excited");
---
+++ 1.SyncAsyncPractise.js
@@ -0,0 +1,4 @@
+console.log("Normal");
+console.log("Normal21");
+setTimeout(()=>console.log("Hello"),2000)
+console.log("Normal2");

25ececfcc805138d50c95f12a14637daaf40b3a6
mdasif8393 <md.asif.8393@gmail.com>
2022-11-01 13:05:51 +0600

    complete

--- 2.Promise.js
+++ 2.Promise.js
@@ -1,7 +1,5 @@
 //pending
-
 //resolved
-
 //rejected
 
 const myPromise = new Promise((resolve, reject) => {
@@ -9,7 +7,6 @@ const myPromise = new Promise((resolve, reject) => {
     if (!user) {
         reject("Something went wrong");
     }
-
     // suppose come data from database
     setTimeout(() => {
         resolve({ name: "John" });
@@ -18,7 +15,6 @@ const myPromise = new Promise((resolve, reject) => {
 
 const userIds = [1, 2, 3, 4, 5];
 let userData = [];
-
 for (let i = 0; i < userIds.length; i++) {
     const userId = userIds[i];
     userData.push(myPromise);
@@ -28,13 +24,10 @@ Promise.all(userData).then(res => {
     console.log(res);
 })
 
-// console.log(userData);
-
 //consume promise
 myPromise
     .then(res => console.log("found in then", res))
     .catch(err => console.log("found in catch", err))
 
-console.log("done");
 
 
